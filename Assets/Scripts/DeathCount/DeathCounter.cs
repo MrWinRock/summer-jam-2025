22735bb1d07f54be9402c47eb850c80582cda648
ratchakit <basszamama20@gmail.com>
2025-04-24 11:07:39 +0700

    fisnish all this shit i'm OUT

--- Assets/Scripts/DeathCount/DeathCounter.cs
+++ Assets/Scripts/DeathCount/DeathCounter.cs
@@ -6,8 +6,9 @@ public class DeathCounter : MonoBehaviour
     public static DeathCounter instance;
     public int deathCount = 0;
 
-    [SerializeField] private string resetSceneName = "Mainmenu"; // <== Change this to your reset scene name
-    private string originalScene;
+    [SerializeField] private int notResetSceneIndex2 = 5; // <== Change this to your reset scene name
+    private int notResetSceneIndex1 = 6; // <== Change this to your not reset scene name
+    private int originalSceneIndex;
 
     void Awake()
     {
@@ -15,7 +16,7 @@ public class DeathCounter : MonoBehaviour
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
-            originalScene = SceneManager.GetActiveScene().name;
+            originalSceneIndex = SceneManager.GetActiveScene().buildIndex;
         }
         else
         {
@@ -26,8 +27,13 @@ public class DeathCounter : MonoBehaviour
     void Update()
     {
         // Only reset if we go to a specific scene (e.g. "MainMenu")
-        if (SceneManager.GetActiveScene().name == resetSceneName)
+        if (SceneManager.GetActiveScene().buildIndex == 5 || SceneManager.GetActiveScene().buildIndex == 6)
         {
+            Debug.Log("Not Resetting Death Count");
+        }
+        else
+        {
+            deathCount = 0; // Reset the death count
             Destroy(gameObject); // this will reset everything next time the original scene is entered
         }
     }
---
+++ Assets/Scripts/Spawn/SpawnBtCollider.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class SpawnBtCollider : MonoBehaviour
+{
+    public List<GameObject> things = new List<GameObject>();
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (GameObject t in things)
+            {
+                t.SetActive(true);
+            }
+        }
+    }
+}
